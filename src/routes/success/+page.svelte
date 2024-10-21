4a25dc73f1edb74c40224498b1b1dd853b4e0d7a
David <davidbuckley12345@gmail.com>
2024-10-21 01:11:33 +0100

    Create Form and Success routes

---
+++ src/routes/success/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="font-bold text-[1.7em] text-center mt-[15%]">Success!</h1>
+<div class="text-center mt-[1.3em]"><a class="font-medium text-center text-[#3f37c9] no-underline hover:no-underline visited:no-underline" href="/form">Go back</a></div>
